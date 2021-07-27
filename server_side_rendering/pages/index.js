75d590c2c4b258c51c0a90a759ca56790993132b
HoKhua22 <khua.ho22@student.passerellesnumeriques.org>
2021-07-27 22:41:13 +0700

    Add content and demo code

---
+++ server_side_rendering/pages/index.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+
+function Home(){
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href='/users'>
+        <a>Users</a>
+      </Link>
+      <Link href='/posts'>
+        <a>Posts</a>
+      </Link>
+    </>
+  )
+  
+}
+
+export default Home
